577c369bc943deae0a32320388a33b432577a1d3
Tian Lan <lantlant917@gmail.com>
2023-02-13 20:08:35 -0500

    init

--- STS/CaresoftInterface/src/com/caresoft/clinicapp/Physician.java
+++ STS/CaresoftInterface/src/com/caresoft/clinicapp/Physician.java
@@ -1,9 +1,56 @@
 package com.caresoft.clinicapp;
 
-public class Physician {
+import java.util.ArrayList;
+import java.util.Date;
 
-	public Physician() {
-		// TODO Auto-generated constructor stub
+public class Physician extends User implements HIPAACompliantUser {
+
+	private ArrayList<String> patientNotes;
+	
+	// TO DO: Constructor that takes an ID
+	public Physician(Integer id) {
+		super(id);
+	
 	}
+       
+    // TO DO: Implement HIPAACompliantUser!
+    public boolean assignPin(int pin) {
+    	int length = String.valueOf(pin).length();
+    	if(length==4) {
+    		super.setPin(pin);
+    		return true;
+    	}else {
+    		return false;
+    	}
+    	
+    }
+    @Override
+    public boolean accessAuthorized(Integer confirmedAuthID) {
+    	if(confirmedAuthID==id) {
+    		return true;
+    	}else {
+    		return false;
+    	}
+
+    }
+	
+
+	public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+
+	// TO DO: Setters & Getters
+	public void setPatientNotes(ArrayList<String> patientNotes) {
+		this.patientNotes = patientNotes;
+	}
+	public ArrayList<String> getPatientNotes() {
+		return patientNotes;
+	}
+	
 
 }
--- STS/CaresoftInterface/src/com/caresoft/clinicapp/User.java
+++ STS/CaresoftInterface/src/com/caresoft/clinicapp/User.java
@@ -4,7 +4,29 @@ public class User {
     protected Integer id;
     protected int pin;
     
-    // TO DO: Getters and setters
-	// Implement a constructor that takes an ID
+    // Implement a constructor that takes an ID
+	public User(Integer id) {
+		this.id = id;
+	}
+
+
+	// TO DO: Getters and setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+      
+	
 }
 
